7f512d5d521b9113d84ede64b5912d39de3d8daf
jdotaz <jdobandoz01@gmail.com>
2020-06-29 17:15:56 -0500

    Resto Chino2

--- Resto Chino/main.cpp
+++ Resto Chino/main.cpp
@@ -1,24 +1,4 @@
-#include <iostream>
-#include <vector>
-#include "invMul.h"
-using namespace std;
-
-int RChino(vector<int> &a,vector<int> &p){
-  vector<int> c,q;
-  int mod=1;
-  for(int i=0;i<p.size();i++){
-    mod*=p[i];
-  }
-  for(int i=0;i<p.size();i++){
-    c.push_back(mod/p[i]);
-    q.push_back(inversaMul(c[i],p[i]));
-  }
-  int res=0;
-  for(int i=0;i<p.size();i++){
-    res+=modulo(a[i]*c[i]*q[i],mod);
-  }
-  return modulo(res,mod);
-}
+#include "RestoChino.h"
 
 int main(){
   //EJEMPLO
